197c824c23e680b916b39c56ecfe953d811f326b
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-09-22 12:45:14 -0300

    Incluyendo un nivel mediante constructor usando un plan...

--- Archivos Ejecutables/js/plataforma.js
+++ Archivos Ejecutables/js/plataforma.js
@@ -10,3 +10,33 @@ let planNivelSimple = `
 ......#++++++++++++#..
 ......##############..
 ......................`;
+
+// THE FOLLOWING CLASS STORES A LEVEL OBJECT. IT'S ARGUMENT SHOULD BE THE STRING THAT DEFINES THE LEVEL...
+
+class Nivel 
+{
+    constructor(plan)
+    {
+        let filas = plan.trim().split("\n").map(l => [...l]);
+        this.altura = filas.length;
+        this.anchura = filas[0].length;
+        this.comienzoActores = [];
+
+        this.filas = filas.map((fila, y) => 
+        {
+            return fila.map((ch, x) =>
+            {
+                let tipo = nivelPersonajes[ch];
+
+                if (typeof tipo != "string")
+                {
+                    let posicion = new Vec(x, y);
+                    this.comienzoActores.push(tipo.create(posicion, ch));
+                    tipo = "empty";
+                }
+
+                return tipo;
+            });
+        });
+    }
+}
